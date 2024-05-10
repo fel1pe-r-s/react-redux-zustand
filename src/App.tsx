9d693a70dbd76927f839088f801ac66aa4463a72
fel1pe-r-s <1felipersilva1@gmail.com>
2024-05-10 17:37:23 -0300

    player

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import "./styles/global.css";
+
+import { Provider as ReduxProvider } from "react-redux";
+
+import { store } from "./store";
+import { Player } from "./pages/Player";
+
 export function App() {
   return (
-    <>
-      <h1>Hello word</h1>
-    </>
+    <ReduxProvider store={store}>
+      <Player />
+    </ReduxProvider>
   );
 }
