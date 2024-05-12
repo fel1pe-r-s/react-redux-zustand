3052bb329b4f580598f3abf195588619feedb5e2
fel1pe-r-s <1felipersilva1@gmail.com>
2024-05-11 21:08:38 -0300

    complete studs redux

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { TypedUseSelectorHook, useSelector } from "react-redux";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { player } from "./slices/player";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: { player },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppDispatch: () => AppDispatch = useDispatch;
